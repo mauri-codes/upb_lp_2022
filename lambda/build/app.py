93fe686da22c422422c4825977a13e4bb217e51d
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2022-06-20 15:22:41 +0000

    fixed post build

--- lambda/build/app.py
+++ lambda/build/app.py
@@ -1,6 +1,7 @@
 import json
+import boto3
+s3 = boto3.client('s3')
 def lambda_handler(event, context):
-    
     return {
         'statusCode': 200,
         'body': json.dumps("hello from lambda")
--- lambda/src/app.py
+++ lambda/src/app.py
@@ -1,6 +1,6 @@
 import json
-# import boto3
-# s3 = boto3.client('s3')
+import boto3
+s3 = boto3.client('s3')
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
